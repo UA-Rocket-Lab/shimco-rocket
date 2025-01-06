9d2e376fdf4ee870066b2b310dad4f4ab8c2df09
Cole Meyer <colemeyer@Coles-MacBook-Pro-2.local>
2025-01-06 14:14:51 -0700

    cleaned up directory in prep for pull request

--- target-selection/scripts/ob-star-visualizer/v1/main.py
+++
@@ -1,9 +0,0 @@
-from layout import app, default_layout
-from callbacks import register_callbacks
-
-app.layout = default_layout()
-
-register_callbacks(app)
-
-if __name__ == '__main__':
-    app.run_server(debug=True)
